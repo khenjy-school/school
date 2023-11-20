0dcd7728a39bb20e6cd31e55e5b4898fe7274f5c
Khenjy Johnelson <71621764+khenjyjohnelson@users.noreply.github.com>
2023-11-20 21:43:11 +0700

    add a new project so my mind wont be empty

--- ukk/pengaduan/application/config/database.php
+++ ukk/pengaduan/application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -78,7 +78,7 @@ $db['default'] = array(
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
-	'database' => 'ukk_pengaduan',
+	'database' => 'school_ukk_pengaduan',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
